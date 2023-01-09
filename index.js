1e5c2147bb6e2f360016ebc82ddb45313e55c451
Aarow Epstein <aarowepstein1@gmail.com>
2023-01-08 20:17:59 -0700

    commit tree 7658ffba4e0bf2c78240e35e387f80abee9e9739

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching (drivers, names) {
+   const findDrivers = drivers.filter( foo => foo.toLowerCase() === names.toLowerCase());
+    return findDrivers;
+}
+
+function fuzzyMatch (drivers, name) {
+    const findDrivers = drivers.filter(
+        driver => driver.slice(0, 1) === name.slice(0, 1)
+        )
+    return findDrivers;
+}
+
+function matchName (drivers, nombre) {
+    const findDrivers = drivers.filter( 
+        driver => driver.name.toLowerCase() === nombre.toLowerCase()
+        );
+    return findDrivers;
+}
